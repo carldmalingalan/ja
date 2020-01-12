2dcf2588f403579578b8955c2daf1c0844aa5842
monkeyuser06 <mmdguzman06@gmail.com>
2020-01-12 13:17:30 +0800

    appointment fixed

--- ajax/appointments.php
+++ ajax/appointments.php
@@ -2,7 +2,13 @@
 
 require_once "../support/ja_config.php";
 
-$test = $conn->query("SELECT AppointmentID, CustomerName, AppointmentDate FROM tblAppointment WHERE AppointmentStatus = 'Pending'")->fetchAll(PDO::FETCH_ASSOC);
+$test = $conn->query(" select a.[JA-Transaction], CustomerName, cast([Date/Time] as Datetime) as Start, 
+DATEADD(mi,DATEPART(MINUTE,MAX(Duration)),DATEADD(hh,DATEPART(HOUR,MAX(Duration)), [Date/Time])) as [End] 
+from tblTransactions a
+inner join tblServicesAvailed b on a.[JA-Transaction] =b.TransactionID
+inner join tblServices c on b.ServiceID = c.ServiceID
+where TransactionStatus = 'For Appointment'
+group by [JA-Transaction],CustomerName,[Date/Time]")->fetchAll(PDO::FETCH_ASSOC);
 
 
 $mainArr = [];
@@ -10,10 +16,10 @@ $mainArr = [];
 
 foreach($test as $key => $val){
     $newObj = new stdClass();
-    $newObj->id = ''.$val["AppointmentID"];
+    $newObj->id = ''.$val["JA-Transaction"];
     $newObj->title = $val["CustomerName"];
-    $newObj->start = $val["AppointmentDate"];
-    $newObj->end = $val["AppointmentDate"];
+    $newObj->start = $val["Start"];
+    $newObj->end = $val["End"];
     array_push($mainArr, $newObj);
 }
 
